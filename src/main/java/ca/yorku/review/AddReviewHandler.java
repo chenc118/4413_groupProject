3f414477ffa60204caa77e9efcf8e3e0d7396b03
chenc118 <chenc118@mcmaster.ca>
2020-12-02 16:57:42 -0500

    Add endpoints for review API

---
+++ src/main/java/ca/yorku/review/AddReviewHandler.java
@@ -0,0 +1,19 @@
+package ca.yorku.review;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.serverless.ApiGatewayResponse;
+
+import java.util.Map;
+
+public class AddReviewHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse>  {
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        ApiGatewayResponse res =  ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody("NewItem")
+                .build();
+        return res;
+
+    }
+}
