c4c257993548bef9afc7d799c11d4ccf59a24943
chenc118 <chenc118@mcmaster.ca>
2020-12-02 18:28:30 -0500

    Add item and implement get and delete handlers

--- src/main/java/ca/yorku/item/AddItemHandler.java
+++ src/main/java/ca/yorku/item/AddItemHandler.java
@@ -1,7 +1,10 @@
 package ca.yorku.item;
 
+import ca.yorku.dal.Item;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.serverless.ApiGatewayResponse;
 
 import java.util.Map;
@@ -10,11 +13,33 @@ public class AddItemHandler implements RequestHandler<Map<String, Object>, ApiGa
 
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-        ApiGatewayResponse res =  ApiGatewayResponse.builder()
-                .setStatusCode(200)
-                .setObjectBody("NewItem")
-                .build();
-        return res;
+
+        try {
+            // get the 'body' from input
+            JsonNode body = new ObjectMapper().readTree((String) input.get("body"));
+
+            Item newItem = new Item();
+            if(body.has("id")) {
+                newItem.setId(body.get("id").asInt());
+            }
+            newItem.setName(body.get("name").asText());
+            if(body.has("price")){
+                newItem.setPrice(body.get("price").asDouble());
+            }
+            if(body.has("quantity")){
+                newItem.setQuantity(body.get("quantity").asLong());
+            }
+            newItem.save();
+            return :ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(newItem)
+                    .build();
+        }catch(Exception e){
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(400)
+                    .setObjectBody("Invalid Input")
+                    .build();
+        }
 
     }
 }
--- src/main/java/ca/yorku/item/GetItemHandler.java
+++ src/main/java/ca/yorku/item/GetItemHandler.java
@@ -1,21 +1,38 @@
 package ca.yorku.item;
 
+import ca.yorku.dal.Item;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.serverless.ApiGatewayResponse;
+import com.serverless.dal.DynamoDBAdapter;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class GetItemHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-        ApiGatewayResponse res =  ApiGatewayResponse.builder()
-                .setStatusCode(200)
-                .setObjectBody("GetItem")
-                .build();
-        return res;
+        Map<String,String> pathParameters =  (Map<String,String>)input.get("pathParameters");
+        int itemId = Integer.parseInt(pathParameters.get("id"));
 
+        Item item = new Item().get(itemId);
+
+
+        if(item != null){
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(item)
+                    .build();
+        }
+        else{
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(404)
+                    .setObjectBody("Item with id: '" + itemId + "' not found.")
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
     }
 
 }
