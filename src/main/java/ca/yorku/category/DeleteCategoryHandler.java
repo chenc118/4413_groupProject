50e64bccc3e7fc5d26697ac573136185902086e8
DBHacks <Daniel.Brown.1396@gmail.com>
2020-12-03 17:54:01 -0500

    Added a handler for delete

---
+++ src/main/java/ca/yorku/category/DeleteCategoryHandler.java
@@ -0,0 +1,32 @@
+package ca.yorku.category;
+
+import ca.yorku.dal.Category;
+import ca.yorku.dal.Item;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.serverless.ApiGatewayResponse;
+
+import java.util.Map;
+
+public class DeleteCategoryHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse>  {
+	@Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		Map<String,String> pathParameters =  (Map<String,String>)input.get("pathParameters");
+		String categoryId = pathParameters.get("Id");
+
+		boolean deleted = new Item().delete(categoryId);
+		if(deleted) {
+			return ApiGatewayResponse.builder()
+					.setStatusCode(200)
+					.setRawBody("OK")
+					.build();
+		}
+		else{
+			return ApiGatewayResponse.builder()
+					.setStatusCode(404)
+					.setRawBody("Item not found")
+					.build();
+		}
+	}
+	
+}
