5a5a1eaa26f5511f647075f87df522fdbd4a944f
DBHacks <Daniel.Brown.1396@gmail.com>
2020-12-03 21:52:33 -0500

    Added analytics handler

---
+++ src/main/java/ca/yorku/analytics/MonthlyReportHandler.java
@@ -0,0 +1,34 @@
+package ca.yorku.analytics;
+
+import ca.yorku.dal.Item;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.serverless.ApiGatewayResponse;
+
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class MonthlyReportHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        Map<String,String> pathParameters =  (Map<String,String>)input.get("pathParameters");
+        String itemId = pathParameters.get("itemId");
+
+        Item item = new Item().get(itemId);
+
+        if(item != null){
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody("itemId: " + item.getId() + ", \n" + "numSold: " + item.getNumSold())
+                    .build();
+        }
+        else{
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(404)
+                    .setObjectBody("item with id: '" + itemId + "' not found.")
+                    .build();
+        }
+    }
+}
