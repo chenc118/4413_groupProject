24cb8a7f7dc67049dfa603f8c82571bf266318a1
chenc118 <chenc118@mcmaster.ca>
2020-12-03 22:08:58 -0500

    Debug log for top 10 query

--- src/main/java/ca/yorku/analytics/GetTop10Report.java
+++ src/main/java/ca/yorku/analytics/GetTop10Report.java
@@ -9,13 +9,18 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.logging.Logger;
 
 public class GetTop10Report implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
         List<Item> allItems = new Item().getAll();
         PriorityQueue<Item> top = new PriorityQueue<>(Comparator.comparingInt(Item::getNumSold));
         for(Item i:allItems){
+            logger.info("Item scan: "+i.getId());
             top.offer(i);
             if(top.size()>10){
                 top.poll();
